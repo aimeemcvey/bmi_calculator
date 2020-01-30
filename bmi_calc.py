ab58ebf345d74547d37a402aaf47e39f0fff0ba0
Aimee McVey <aimee.mcvey@duke.edu>
2020-01-30 00:16:11 -0500

    Adds weight and height input options

--- bmi_calc.py
+++ bmi_calc.py
@@ -9,8 +9,24 @@ def interface():
         print("  2 - Imperial (lb/in)")
         print("  9 - Quit")
         choice = input("Enter your option: ")
-        if choice == '9':
+        if choice == '1':
+            metric_analysis()
+        elif choice == '2':
+            imperial_analysis()
+        elif choice == '9':
             return
+
+def data_input():
+    weight_input = input("Enter weight: ")
+    height_input = input("Enter height: ")
+
+
+def metric_analysis():
+    data_input()
+
+
+def imperial_analysis():
+    data_input()
 
 
 if __name__ == "__main__":
